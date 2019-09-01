fd6a0b33d94123cba37056175f3da2b07be127f4
boombelka <av_cons@inbox.ru>
2019-09-01 08:01:59 +0300

    Изменено название файла на вменяемое. Написаны наметки процедур

---
+++ game.py
@@ -0,0 +1,26 @@
+import json
+import menu
+
+class Profile(object):
+
+    def __init__ (self):
+        self.nic = nic is str
+        self.name = name is str
+        self.password = password
+
+palyers = []
+gameover = False
+while gameover is not False:
+    menu()
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    print('Запущена вручную')
+else:
+    ('Запущена автоматически')
