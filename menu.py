d15dd531f8392e3e8c190f7d6d0e8332421df4d2
boombelka <av_cons@inbox.ru>
2019-09-02 02:56:28 +0300

    Создано меню

---
+++ menu.py
@@ -0,0 +1,24 @@
+import json
+import game
+class Menu(object):
+
+    def __init__(self):
+        pass
+
+def menu():
+    #name_str = []
+    print('Добро пожаловать в игру Magth and Magic text')
+    while True:
+        pass
+ #       if
+ #       name_str[i] = input('Введите пожалуйста ваше имя:\n')
+
+    pass
+
+
+def load_game():
+    pass
+
+
+def save_game():
+    pass
